a15d6f07ad103b7746c8cbcbc4412e1af6012d85
Newik83CZ <paveprac@gmail.com>
2025-12-21 21:34:40 +1030

    Changed font size for phone

--- src/pages/Lineups.jsx
+++ src/pages/Lineups.jsx
@@ -56,7 +56,7 @@ function DraggablePlayer({ id, label, sublabel, preferredPosition, isError = fal
   const isMobile = typeof window !== "undefined" && window.matchMedia("(max-width: 800px)").matches;
 
   const lastNameFixed =
-    labelObj.lastName && labelObj.lastName.length <= 8 ? (isMobile ? 14 : 18) : null;
+    labelObj.lastName && labelObj.lastName.length <= 7 ? (isMobile ? 14 : 18) : null;
 
   useLayoutEffect(() => {
     const rowEl = rowRef.current;
